af2c59c0def21797ea9683f8929fffead709e0b8
MiaoQiaowei <252544058@qq.com>
2020-11-15 14:47:39 +0800

    done

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Page - Article Header Image (Immersive + Translucent Header)",'url':"/2020/10/20/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome！",'url':"/2020/10/20/welcome.html"}]};
