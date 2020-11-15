bb7dd4cab1fa8ccca752c53303770e58258d0166
MiaoQiaowei <252544058@qq.com>
2020-11-15 14:59:37 +0800

    done

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome！",'url':"/2020/10/20/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome!",'url':"/2020/10/20/welcome.html"}]};
