24c8888196ffb4f66209d09af7b634ead076cd91
MiaoQiaowei <252544058@qq.com>
2020-11-15 15:44:53 +0800

    done

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome!",'url':"/2020/10/20/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome!",'url':"/2020/10/20/welcome.html"},{'title':"念念不忘，必有回响！",'url':"/2020/11/15/iOS%E7%BB%84%E6%88%98%E6%8A%A5.html"}]};
